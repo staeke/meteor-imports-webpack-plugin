f3560d9ecdf373b1abee96ef6cfc6b25104be8fc
Luis Herranz <luisherranz@gmail.com>
2016-03-18 19:28:40 +0100

    Everything works but needs more testing.

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
+var fs = require('fs');
 var path = require('path');
 
 function MeteorImportsPlugin(config) {
-  config.EXCLUDE_REGEXP = new RegExp([
+  config.EXCLUDE = [
     'autoupdate',
     'global-imports',
     'hot-code-push',
     'reaload',
-  ].concat(config.EXCLUDE || []).join('|'));
+  ].concat(config.EXCLUDE || []);
   this.config = config;
 }
 
 MeteorImportsPlugin.prototype.apply = function(compiler) {
   var self = this;
+
   compiler.plugin("compile", function(params) {
     var meteorBuild = path.join(
       params.normalModuleFactory.context, self.config.METEOR_FOLDER,
       '.meteor', 'local', 'build', 'programs', 'web.browser'
     );
+
+    if (compiler.options.module.loaders === undefined)
+      throw Error('Add an empty array in module.loaders of your webpack config.');
+
+    try {
+      fs.accessSync(meteorBuild + '/program.json', fs.F_OK);
+    } catch (e) {
+      throw Error('Run your Meteor server at least once.')
+    }
+
     // Create an alias so we can do the context properly using the folder
     // variable from the meteor config file. If we inject the folder variable
     // directly in the request.context webpack goes wild.
@@ -27,10 +39,9 @@ MeteorImportsPlugin.prototype.apply = function(compiler) {
       __dirname, './meteor-imports.js');
 
     // Add a loader to inject the meteor config in the meteor-init script.
-    compiler.options.module.loaders = compiler.options.module.loaders || [];
     compiler.options.module.loaders.push({
-      test: /meteor-imports/,
-      loader: 'imports?config=>' + JSON.stringify(self.config),
+      test: /meteor-config/,
+      loader: 'json-string-loader?json=' + JSON.stringify(self.config)
     });
 
     // Add Meteor packages like if they were NPM packages.
@@ -42,7 +53,7 @@ MeteorImportsPlugin.prototype.apply = function(compiler) {
   compiler.plugin("normal-module-factory", function(nmf) {
 		nmf.plugin("before-resolve", function(result, callback) {
 			if(!result) return callback();
-			if(self.config.EXCLUDE_REGEXP.test(result.request)) {
+			if(new RegExp(self.config.EXCLUDE.join('|')).test(result.request)){
 				return callback();
 			}
 			return callback(null, result);
--- meteor-imports.js
+++ meteor-imports.js
@@ -1,6 +1,12 @@
-debugger;
+try {
+  var manifest = require('json!meteor-build/program.json').manifest;
+  // manifest = JSON.stringify(configJson).manifest;
+} catch(err) {
+  console.error('Error: You have to run Meteor at least once.');
+}
 
-var manifest = require('json!meteor-build/program.json').manifest;
+var config = require('./meteor-config.json');
+
 __meteor_runtime_config__ = config;
 
 var req = require.context(
@@ -9,10 +15,12 @@ var req = require.context(
   /\.js$/
 );
 
-var excluded = new RegExp(config.exclude
+var excluded = new RegExp(config.EXCLUDE
   .map(function(exclude){ return 'packages/' + exclude + '.js'; }).join('|'));
 
 manifest.forEach(function(package){
   if (!excluded.test(package.path))
     req('./' + package.path);
 });
+
+debugger;
