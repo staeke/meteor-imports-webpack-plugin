45aab10a93599bd3ed385ea0b7fe7143cbda3b4f
Luis Herranz <luisherranz@gmail.com>
2016-03-20 21:24:43 +0100

    Fixed inclusion of app meteor files, instead of only packages.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var path = require('path');
 
 function MeteorImportsPlugin(config) {
@@ -15,14 +14,17 @@ MeteorImportsPlugin.prototype.apply = function(compiler) {
   var self = this;
 
   compiler.plugin("compile", function(params) {
+    // Create path for internal build of the meteor packages.
     var meteorBuild = path.join(
       params.normalModuleFactory.context, self.config.METEOR_FOLDER,
       '.meteor', 'local', 'build', 'programs', 'web.browser'
     );
 
+    // Check if module loaders is defined.
     if (compiler.options.module.loaders === undefined)
       throw Error('Add an empty array in module.loaders of your webpack config.');
 
+    // Check if Meteor has been run at least once.
     try {
       var manifest = require(meteorBuild + '/program.json').manifest;
     } catch (e) {
@@ -33,35 +35,40 @@ MeteorImportsPlugin.prototype.apply = function(compiler) {
     // variable from the meteor config file. If we inject the folder variable
     // directly in the request.context webpack goes wild.
     compiler.options.resolve.alias['meteor-build'] = meteorBuild;
+    compiler.options.resolve.alias['meteor-packages'] = meteorBuild+'/packages';
 
-    // Create an alias for the meteor-init script.
+    // Create an alias for the meteor-imports require.
     compiler.options.resolve.alias['meteor-imports'] = path.join(
       __dirname, './meteor-imports.js');
 
-    // Add a loader to inject the meteor config in the meteor-init script.
+    // Add a loader to inject the meteor config in the meteor-imports require.
     compiler.options.module.loaders.push({
       test: /meteor-config/,
       loader: 'json-string-loader?json=' + JSON.stringify(self.config)
     });
 
-    // Add a loader to inject the meteor config in the meteor-init script.
+    // Add a loader to inject this as window in the meteor packages.
     compiler.options.module.loaders.push({
       test: new RegExp('.meteor/local/build/programs/web.browser/packages'),
       loader: 'imports?this=>window'
     });
 
+    // Add a resolveLoader to use the loaders from this plugin's own NPM
+    // dependencies.
     compiler.options.resolveLoader.modulesDirectories.push(
       path.join(__dirname, 'node_modules')
     );
-
 
     // Add Meteor packages like if they were NPM packages.
     compiler.options.resolve.modulesDirectories.push(
       path.join( meteorBuild, 'packages'));
 
-    // Create alias for all the packages.
+    // Create an alias for each Meteor packages and a loader to extract its
+    // globals.
     var excluded = new RegExp(self.config.EXCLUDE
-      .map(function(exclude){ return 'packages/' + exclude + '.js'; }).join('|'));
+      .map(function(exclude){ return '^packages/' + exclude + '.js$'; })
+      .concat('^app\/.+.js$')
+      .join('|'));
     manifest.forEach(function(package){
       if (!excluded.test(package.path)) {
         var packageName = /^packages\/(.+)\.js$/.exec(package.path)[1];
--- meteor-imports.js
+++ meteor-imports.js
@@ -5,12 +5,16 @@ __meteor_runtime_config__ = config;
 
 // Create context to create a chunk for each Meteor package.
 var req = require.context(
-  'meteor-build', true, /\.js$/);
+  'meteor-packages', false, /\.js$/);
+
+// Create regexp to exclude the packages we don't want.
+var excluded = new RegExp(config.EXCLUDE
+  .map(function(exclude){ return '^packages/' + exclude + '.js$'; })
+  .concat('^app\/.+.js$')
+  .join('|'));
 
 // Require the Meteor packages.
-var excluded = new RegExp(config.EXCLUDE
-  .map(function(exclude){ return 'packages/' + exclude + '.js'; }).join('|'));
 manifest.forEach(function(package){
   if (!excluded.test(package.path))
-    req('./' + package.path);
+    req('./' + package.path.replace('packages/', ''));
 });
