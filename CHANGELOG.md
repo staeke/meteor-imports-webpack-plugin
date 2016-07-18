73014cc1a3ab23b4beeaae31047beffa2261be13
Luis Herranz <luisherranz@gmail.com>
2016-07-18 09:43:21 +0200

    Fix for undefined __meteor_runtime_config__.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 Change History
 ==============
 
+v1.0.6
+----
+* Fix for undefined __meteor_runtime_config__.
+
 v1.0.5
 ----
 * @adam187 fix for HMR.
--- meteor-imports.js
+++ meteor-imports.js
@@ -1,7 +1,7 @@
 var manifest = require('json!meteor-build/program.json').manifest;
 var config = require('./meteor-config.json');
 
-__meteor_runtime_config__ = config;
+window.__meteor_runtime_config__ = config;
 
 // Create context to create a chunk for each Meteor package.
 var req = require.context(
