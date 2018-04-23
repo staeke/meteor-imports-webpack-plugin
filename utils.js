4b4d376765eb61f21437be35d4751dea315accbd
Staffan Eketorp <staffan.eketorp@gmail.com>
2018-04-23 10:38:25 +0200

    util log methods

--- utils.js
+++ utils.js
@@ -1,5 +1,15 @@
 const path = require('path');
+const colors = require('colors/safe');
 
 module.exports = {
-  getFileNameWoExt: file => path.basename(file, path.extname(file))
+  getFileNameWoExt: file => path.basename(file, path.extname(file)),
+  log(...args) {
+    console.log('MeteorImportsWebpackPlugin:', ...args);
+  },
+  logWarn(msg, ...args) {
+    console.log(colors.yellow('MeteorImportsWebpackPlugin:'), colors.yellow(msg), ...args);
+  },
+  logError(msg, ...args) {
+    console.log(colors.red('MeteorImportsWebpackPlugin:'), colors.red(msg), ...args);
+  },
 };
